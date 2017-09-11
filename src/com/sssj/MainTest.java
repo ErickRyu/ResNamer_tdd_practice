a796e258aafa0b7ab822295a66672e26e4106988
Ian <developer.kss@gmail.com>
2017-09-11 23:20:55 +0900

    Paths 도입

--- src/com/sssj/MainTest.java
+++ src/com/sssj/MainTest.java
@@ -11,39 +11,38 @@ public class MainTest {
 
     @Test
     public void target이없을경우source를target으로사용한다() throws Exception {
-        String[] result = folderResolver("aaa", "");
-        String source = result[0];
-        String target = result[1];
-
-        assertEquals(source, target);
+        Paths result = folderResolver("aaa", "");
+        assertEquals(result.source, result.target);
     }
 
     @Test
     public void target과source가같이들어오면각각을사용한다() {
         final String source = "aaa";
         final String target = "bbb";
-        String[] result = folderResolver(source, target);
-        final String source2 = result[0];
-        final String target2 = result[1];
-        assertEquals(source, source2);
-        assertEquals(target, target2);
+        Paths result = folderResolver(source, target);
+        assertEquals(source, result.source);
+        assertEquals(target, result.target);
     }
 
     @Test
     public void source가없으면_현재폴더를_source로_사용한다() {
         String source = "";
         String target = "";
-        String[] result = folderResolver(source, target);
-        String source2 = result[0];
-        String target2 = result[1];
+        Paths result = folderResolver(source, target);
         String currentPath = getCurrentPath();
-        assertEquals(currentPath, source2);
-        assertEquals(currentPath, target2);
+        assertEquals(currentPath, result.source);
+        assertEquals(currentPath, result.target);
     }
 
     static class Paths{
         public String source;
         public String target;
+
+        public Paths(String source, String target) {
+            this.source = source;
+            this.target = target;
+        }
+
     }
 
     private String getCurrentPath() {
@@ -55,13 +54,13 @@ public class MainTest {
     }
 
 
-    private String[] folderResolver(String source, String target) {
+    private Paths folderResolver(String source, String target) {
         if (source.isEmpty()) {
-            return new String[]{getCurrentPath(), getCurrentPath()};
+            return new Paths(getCurrentPath(), getCurrentPath());
         }
         if (target.isEmpty()) {
-            return new String[]{source, source};
+            return new Paths(source, source);
         }
-        return new String[]{source, target};
+        return new Paths(source, target);
     }
 }
